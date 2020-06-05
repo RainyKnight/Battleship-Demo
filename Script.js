80e0908d58ad04208bb4144471a0fd0c3177c8d3
Lucas Stoltman <lucasstoltman@Lucass-MacBook-Pro.local>
2020-06-05 12:29:00 -0700

    Added hit-detection functionality

--- Script.js
+++ Script.js
@@ -1,5 +1,5 @@
 function runCode() {
-runGame();
+    runGame();
 }
 
 function runGame() {
@@ -7,7 +7,7 @@ function runGame() {
     // TODO create a random location for the ship
     var location1 = 3;
     var location2 = 4;
-    var location3 = 5;  
+    var location3 = 5;
 
     var guess;
     var hits = 0;
@@ -15,28 +15,27 @@ function runGame() {
 
     var isSunk = false;
 
-    while (isSunk == false){
+    while (isSunk == false) {
 
         // prompt sends an alert with a text input box. If nothing is entered, the value is "null"
         guess = prompt("Ready, aim, fire! (enter a number from 0-6)")
 
-        // compare user input to values
-
         if (guess < 0 || guess > 6) {
-            alert ("Invalid input. \nPlease enter a number from 0-6.")
+            alert("Invalid input. \nPlease enter a number from 0-6.")
         }
-        else{
+        else {
             numGuesses++;
-            if (guess == (location1 || location2 || location3)){
+            if (guess == location1 || guess == location2 || guess == location3) {
                 hits++;
-                if (hits == 3){
-                    isSunk = true;
-                    alert("You sank my battleship!");
-                }
+            }
+
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battleship!");
             }
         }
     }
 
-    // stats
-    alert ("It took you " + numGuesses + " guesses to sink the ship!")
+// stats
+alert("It took you " + numGuesses + " guesses to sink the ship!")
 }
